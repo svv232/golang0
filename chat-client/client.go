aa2343cccf459c1c654ec24a06586a6045e1de6a
Sai <svv232@nyu.edu>
2017-12-21 17:08:49 -0500

    added an initial Dockerfile

--- chat-client/client.go
+++ chat-client/client.go
@@ -1,13 +1,8 @@
 package main
 
 import "fmt"
-import "sort"
 
 
 func main(){
-    nums := []int{9,2,4,39,34,32,2}
-    sort.Ints(nums)
-    fmt.Println("Ints:  ", nums)
-    s := sort.IntsAreSorted(nums)
-    fmt.Println("Sorted:", s)
+    fmt.Println("Hello World")
 }
