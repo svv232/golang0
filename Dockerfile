bd785e1f43d6e7b214c068812ecea1a5c3da4467
Sai <svv232@nyu.edu>
2017-12-23 23:20:40 -0500

    started basic client outline with no security and stdin along with no channels

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ LABEL Description="A chat platform by Sai Vegasena"
 #installation 
 RUN apt-get clean 
 RUN apt-get update && apt-get upgrade -y
+RUN apt-get install golang
 
 #user 
 WORKDIR /app
--- chat-client/client.go
+++ chat-client/client.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+"fmt"
+)
 
+type user struct{
+    id, authcode int
+    name string
+}
+
+type Session struct {
+    users map[string]*user
+}
+
+func (u user) chat(message string){
+    fmt.Println(u.name + " :" + message)
+}
+
+func newSess() *Session{
+    var sess Session
+    sess.users = make(map[string]*user)
+    return &sess
+}
+
+func (s Session) addUser(name string){
+    authcode := 1234
+    s.users[name] = &user{len(s.users), authcode, name}
+}
 
 func main(){
-    fmt.Println("Hello World")
+    sess := newSess()
+    sess.addUser("sai")
+    sess.addUser("tnek")
+    sess.users["sai"].chat("hey doo")
+    sess.users["tnek"].chat("WazZoop!")
 }
