f08be9eacaf70a5c97b9648f6f91e91161b67d76
Sai <svv232@nyu.edu>
2017-12-25 20:28:54 -0500

    fixing client

--- chat-client/client.go
+++ chat-client/client.go
@@ -2,36 +2,58 @@ package main
 
 import (
 "fmt"
+//"sync"
+//"crypto/sha1"
 )
 
-type user struct{
-    id, authcode int
-    name string
-}
+/*************************************************/
+var stream chan string = make(chan string, 1)
 
 type Session struct {
     users map[string]*user
 }
 
-func (u user) chat(message string){
-    fmt.Println(u.name + " :" + message)
+type user struct{
+    id, authcode int
+    name string
 }
-
+/*************************************************/
+func (u user) chat(message string) string{
+    return u.name + " :" + message
+}
+/*************************************************/
 func newSess() *Session{
     var sess Session
     sess.users = make(map[string]*user)
     return &sess
 }
 
-func (s Session) addUser(name string){
+func (s *Session) addUser(name string){
     authcode := 1234
     s.users[name] = &user{len(s.users), authcode, name}
 }
 
+func (s *Session) userChat(message string, name string) {
+    if _, ok := s.users["name"]; ok{
+        stream <- s.users[name].chat(message)
+    }
+}
+
+/*************************************************/
 func main(){
     sess := newSess()
-    sess.addUser("sai")
-    sess.addUser("tnek")
-    sess.users["sai"].chat("hey doo")
-    sess.users["tnek"].chat("WazZoop!")
+    names := [4]string{"sai","tnek","captiosus", "_passion"}
+    for i := range names{
+        sess.addUser(names[i])
+    }
+
+    phrases := [4]string{"zippo!", "twits", "tits", "heyooo"}
+    for i:=0; i < len(phrases); i++{
+        sess.addUser(names[i])
+        sess.userChat(phrases[i], names[i])
+    }
+
+    msg := <-stream
+    close(stream)
+    fmt.Scanln(msg)
 }
